13f3d283c79fa8d9ed1119c4ade9979d53169d64
Carlos Hernandez Santos <carlos@hernandezsantos.com>
2021-01-07 14:13:55 +0100

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useState, useEffect } from "react";
+
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setisLoading(false);
+      }
+      .catch((err) => console.log(err));
+  });
   return (
-    <div class="max-w-sm rounded overflow-hiden shadow-lg">
+    <div className="max-w-sm rounded overflow-hiden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full" />
       <div className="px-6 py-4">
-        <div class="font-bold text-purple-500 text-xl mb-2">
+        <div className="font-bold text-purple-500 text-xl mb-2">
           Photo by John Doe
         </div>
         <ul>
@@ -22,13 +39,13 @@ function App() {
         </ul>
       </div>
       <div className="px-6 py-4">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py 1 text-sm font-semibold text-gray-700 mr-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py 1 text-sm font-semibold text-gray-700 mr-2">
           #tag1
         </span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py 1 text-sm font-semibold text-gray-700 mr-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py 1 text-sm font-semibold text-gray-700 mr-2">
           #tag2
         </span>
-        <span class="inline-block bg-gray-200 rounded-full px-3 py 1 text-sm font-semibold text-gray-700 mr-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py 1 text-sm font-semibold text-gray-700 mr-2">
           #tag3
         </span>
       </div>
